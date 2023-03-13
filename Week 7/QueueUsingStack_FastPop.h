e0bcffd3570d948a72f7db791ed716760d9b5bea
kgedt <magzumov7162@gmail.com>
2023-03-14 02:30:28 +0600

    explanations added

--- Week 7/QueueUsingStack_FastPop.h
+++ Week 7/QueueUsingStack_FastPop.h
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-class QueueUsingStack_FastPop {
+class QueueUsingStack_FastPop { // В ЭТОЙ ОЧЕРЕДИ FRONT() ЭЛЕМЕНТ НАХОДИТСЯ СВЕРХУ СТЭКА. BACK() - СНИЗУ
 private:
-    stack<int> s1;
-    stack<int> s2;
+    stack<int> s1; // основной стэк, где хранится очередь
+    stack<int> s2; // вспомогательный стэк, чтобы проворачивать приколы с push() и pop()
 
 public:
     QueueUsingStack_FastPop() {
@@ -16,58 +16,72 @@ public:
         s2 = stack<int>();
     }
 
-    void push(int element) { // front() element at the top of the stack
-        if (s1.empty()) {
+    void push(int element) {
+        if (s1.empty()) { // если стэк пустой, просто пушаем
             s1.push(element);
             return;
         }
-        while (!s1.empty()) {
+
+        // самый первый элемент находится сверху стэка
+        // следовательно, последний нужно добавлять в конец стэка
+
+        while (!s1.empty()) { // для этого перемещаем все элементы сначала во 2 стэк
             s2.push(s1.top());
             s1.pop();
         }
 
-        s2.push(element);
+        s2.push(element); // добавляем наверх 2 стэка элемент, который мы хотим добавить
+        // при добавлении элемента наверх 2 стэка
+        // он окажется последним в 1 стэке => последним в queue
 
-        while (!s2.empty()) {
+        while (!s2.empty()) { // как только добавили элемент, который хотели, перемещаем все в 1-й
             s1.push(s2.top());
             s2.pop();
         }
     }
 
     void pop() {
-        s1.pop();
+        s1.pop(); // поскольку front элемент находится сверху стэка
+        // достаточно удалить его и все
     }
 
-    int front() {
+    int front() { // FIFO (первый вошел, первый вышел. как в трубу короче)
+        // так как первый элемент очереди сверху стэка, то достаточно вывести stack.top()
+
+        if (s1.empty()) { // если стэк пустой, то и очередь пустая
+            cout << "stack is empty!\n";
+            return NULL;
+        }
+        return s1.top(); //
+    }
+
+    int back() { // задний элемент находится снизу стэка
+        // нужно сначала освободить все вышестоящие элементы, чтобы дойти до нижнего
+
         if (s1.empty()) {
             cout << "stack is empty!\n";
             return NULL;
         }
-        return s1.top();
-    }
 
-    int back() {
-        if (s1.empty()) {
-            cout << "stack is empty!\n";
-            return NULL;
-        }
-        int back_element;
-        while (s1.size() > 1) {
+        int back_element; // запоминаем последний элемент очереди, потому что..
+        // нам нужно еще вернуть все элементы в 1 стэк, который и является по сути очередью
+
+        while (s1.size() > 1) { // перемещаем все элементы во 2-й стэк, пока не дойдем до нижнего
             s2.push(s1.top());
             s1.pop();
         }
 
-        back_element = s1.top();
+        back_element = s1.top(); // дошли до низа стэка и записываем в переменную
 
-        while (!s2.empty()) {
+        while (!s2.empty()) { // возвращаем всё из 2-го стэка в 1-й(ПОВТОРЮСЬ: 1-й СТЭК И ЕСТЬ НАША ОЧЕРЕДЬ ПО СУТИ)
             s1.push(s2.top());
             s2.pop();
         }
 
-        return back_element;
+        return back_element; // ну и собсна возвращаем тот задний элемент, который запомнили
     }
 
-    void printQueue() {
+    void printQueue() { // вывод очереди
         while (!s1.empty()) {
             s2.push(s1.top());
             s1.pop();
--- Week 7/QueueUsingStack_FastPush.h
+++ Week 7/QueueUsingStack_FastPush.h
@@ -5,10 +5,12 @@
 
 using namespace std;
 
-class QueueUsingStack_FastPush {
+class QueueUsingStack_FastPush { // FRONT() элемент находится внизу стэка, а задний - сверху
+                                // можно догадаться, что в этом случае проще будет получать задний элемент
+                                // а еще пушить в очередь еще один элемент
 private:
-    stack<int> s1;
-    stack<int> s2;
+    stack<int> s1; // основной стэк, где хранится очередь
+    stack<int> s2; // вспомогательный стэк, чтобы проворачивать приколы с push() и pop()
 
 public:
     QueueUsingStack_FastPush() {
@@ -16,7 +18,8 @@ public:
         s2 = stack<int>();
     }
 
-    void push(int element) { // front() element at the end of the stack
+    void push(int element) { // так как фронт элемент находится снизу, то задний будет сверху
+                            // просто кидаем его наверх стэка
         s1.push(element);
     }
 
@@ -25,11 +28,17 @@ public:
             cout << "queue is empty!\n";
             return;
         }
-        while (s1.size() > 1) {
+        while (s1.size() > 1) { // чтобы удалить из очереди элемент front, нужно дойти до него
+                                // он, как я и говорил, находится снизу стэка
+                                // поэтому опустошаем(перемещаем временно во 2-й) до последнего элемента
             s2.push(s1.top());
             s1.pop();
         }
+
+        // как только в первом стэке остается один front элемент, то удаляем его к чертям
         s1.pop();
+
+        // возвращем из 2-го все обратно в 1-й стэк
         while (!s2.empty()) {
             s1.push(s2.top());
             s2.pop();
@@ -41,28 +50,32 @@ public:
             cout << "queue is empty!\n";
             return NULL;
         }
-        int front_element;
+        int front_element; // переменная, которую мы вернем как front()
+
+        // в последний раз повторюсь, что front элемент(который первый зашел и первым выйдет)
+        // находится снизу стэка
+        // значит освобождаем все выше него
         while (s1.size() > 1) {
             s2.push(s1.top());
             s1.pop();
         }
 
-        front_element = s1.top();
+        front_element = s1.top(); // дошли до последнего и запоминаем его
 
-        while (!s2.empty()) {
+        while (!s2.empty()) { // возвращаем все из 2-го обратно в 1-й
             s1.push(s2.top());
             s2.pop();
         }
 
-        return front_element;
+        return front_element; // возвращаем тот, что запомнили как front element
     }
 
-    int back() {
+    int back() { // ну тут попроще. задний элемент всегда сверху, поэтому..
         if (s1.empty()) {
             cout << "stack is empty!\n";
             return NULL;
         }
-        return s1.top();
+        return s1.top(); // поэтому возвращаем просто верхний элемент 1-го стэка
     }
 
     void printQueue() {
